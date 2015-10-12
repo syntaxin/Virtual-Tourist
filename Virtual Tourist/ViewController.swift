c1ced236caf28b81a00038f367fef725d77b2ac6
syntaxin <syntaxin.github@gmail.com>
2015-10-11 23:53:39 -0700

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Enrico Montana on 10/11/15.
+//  Copyright © 2015 Enrico Montana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
